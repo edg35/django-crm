91dfa39f1e905d98f5176c11bd8ba02c7b8a29d6
Ethan Green <66663131+edg35@users.noreply.github.com>
2024-06-03 00:02:33 -0400

    finalized leads

--- leads/models.py
+++ leads/models.py
@@ -7,18 +7,17 @@ class User(AbstractUser):
 
 
 class Lead(models.Model):
-
-    SOURCE_CHOICES = (
-        ('YouTube', 'YouTube'),
-        ('Google', 'Google'),
-        ('Newsletter', 'Newsletter'),
-    )
-
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
 
 class Agent(models.Model):
     user = models.OneToOneField("User", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user.email

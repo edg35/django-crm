8823c22c2b44d8e08fadfcbaa3a0a9288dd3b97c
Ethan Green <66663131+edg35@users.noreply.github.com>
2024-06-04 12:30:41 -0400

    urls

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,13 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Lead
+
+
+def home_page(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads
+    }
+
+    return render(request, "leads/home_page.html", context)
